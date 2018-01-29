75ff591aac6a4abe0a17ef1d2db63cf1282a7dee
Heum Sinat <heumsinatgic25@gmail.com>
2018-01-29 11:57:09 +0700

    fix on section page

--- src/pages/section/section.ts
+++ src/pages/section/section.ts
@@ -36,12 +36,33 @@ export class SectionPage {
               created_date:res.rows.item(i).created_date,
               modified_date:res.rows.item(i).modified_date
             })
+            //break;
           }
         }).catch(e => console.log((e)))
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad SectionPage');
+  }
+
+  next_section(){
+    // this.sections = [];
+    // for (var i = 0; i<res.rows.length; i++){
+    //   this.sections.push({
+    //     id:res.rows.item(i).id,
+    //     order:res.rows.item(i).order,
+    //     lesson:res.rows.item(i).lesson,
+    //     title:res.rows.item(i).title,
+    //     content:res.rows.item(i).content,
+    //     image1:res.rows.item(i).image1,
+    //     image2:res.rows.item(i).image2,
+    //     image3:res.rows.item(i).image3,
+    //     image4:res.rows.item(i).image4,
+    //     sound:res.rows.item(i).sound,
+    //     created_date:res.rows.item(i).created_date,
+    //     modified_date:res.rows.item(i).modified_date
+    //   })
+    // }
   }
 
 }
